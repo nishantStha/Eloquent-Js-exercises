0a4e21de63bdc7b2fd5f495b9b6a4e60137f1518
nishantStha <nishant19shrestha@gmail.com>
2021-10-24 23:45:15 +0545

    Create arrayToList.js

---
+++ Chapter_4/arrayToList.js
@@ -0,0 +1,14 @@
+function arrayToList(array) 
+{
+    let list = null;
+    for(let i = array.length - 1; i >= 0; i--)
+    {
+        list = {
+            value : array[i],
+            rest: list  
+        };
+    }
+
+    return list;
+}
+console.log(JSON.stringify(arrayToList([1,2,3])))
